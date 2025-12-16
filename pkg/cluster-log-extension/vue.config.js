e508879d338466ccb67f2c5bc8cbf744311ae2b4
mgrinbergpractiauy <130935328+mgrinbergpractiauy@users.noreply.github.com>
2025-12-16 18:55:00 -0300

    Update vue.config.js

--- pkg/cluster-log-extension/vue.config.js
+++ pkg/cluster-log-extension/vue.config.js
@@ -1,13 +1,9 @@
-const ENTRY_FILE = './index.ts';
+const path = require('path');
 
-// Reemplaza la línea vieja por esto:
 module.exports = require('../../node_modules/@rancher/shell/pkg/vue.config')(__dirname, {
-  // Aquí se le dice a Vue que el archivo de entrada de tu extensión es index.ts, no src/main.ts
-  pages: {
-    index: {
-      entry: ENTRY_FILE,
-      // template: 'public/index.html', // Generalmente no se necesita en extensiones
-      filename: 'index.html'
-    }
+  chainWebpack(config) {
+    config.resolve.alias
+      .set('@shell', path.resolve(__dirname, '../../node_modules/@rancher/shell/lib'))
+      .set('~@shell', path.resolve(__dirname, '../../node_modules/@rancher/shell/lib'));
   }
 });
