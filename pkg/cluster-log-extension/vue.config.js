3c71f917efd956e5d084b2317b13f0d6a5f07474
mgrinbergpractiauy <130935328+mgrinbergpractiauy@users.noreply.github.com>
2025-12-16 19:03:02 -0300

    Update vue.config.js

--- pkg/cluster-log-extension/vue.config.js
+++ pkg/cluster-log-extension/vue.config.js
@@ -1,9 +1,13 @@
+// pkg/cluster-log-extension/vue.config.js
 const path = require('path');
+const CWD = path.resolve(__dirname, '..', '..', '..'); // Tres niveles arriba (rancher root)
 
 module.exports = require('../../node_modules/@rancher/shell/pkg/vue.config')(__dirname, {
   chainWebpack(config) {
     config.resolve.alias
-      .set('@shell', path.resolve(__dirname, '../../node_modules/@rancher/shell'))
-      .set('~@shell', path.resolve(__dirname, '../../node_modules/@rancher/shell'));
+      // Apuntamos al directorio node_modules/ en la raíz (CWD),
+      // ya que así es como Webpack a menudo lo resuelve en Rancher.
+      .set('@shell', path.resolve(CWD, 'node_modules/@rancher/shell'))
+      .set('~@shell', path.resolve(CWD, 'node_modules/@rancher/shell'));
   }
 });
