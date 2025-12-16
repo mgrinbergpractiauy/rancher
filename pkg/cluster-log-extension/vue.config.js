5fd4d28bee44a94c955c6fed072d94a63a6ca017
mgrinbergpractiauy <130935328+mgrinbergpractiauy@users.noreply.github.com>
2025-12-16 19:00:27 -0300

    Update vue.config.js

--- pkg/cluster-log-extension/vue.config.js
+++ pkg/cluster-log-extension/vue.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = require('../../node_modules/@rancher/shell/pkg/vue.config')(__dirname, {
   chainWebpack(config) {
     config.resolve.alias
-      .set('@shell', path.resolve(__dirname, '../../node_modules/@rancher/shell/lib'))
-      .set('~@shell', path.resolve(__dirname, '../../node_modules/@rancher/shell/lib'));
+      .set('@shell', path.resolve(__dirname, '../../node_modules/@rancher/shell'))
+      .set('~@shell', path.resolve(__dirname, '../../node_modules/@rancher/shell'));
   }
 });
