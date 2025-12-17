4229a8cd74c9574bfd3b1b2f8251ffdffb91747f
mgrinbergpractiauy <130935328+mgrinbergpractiauy@users.noreply.github.com>
2025-12-17 09:21:32 -0300

    Update vue.config.js

--- pkg/cluster-log-extension/vue.config.js
+++ pkg/cluster-log-extension/vue.config.js
@@ -1,16 +1,24 @@
-// pkg/cluster-log-extension/vue.config.js
 const path = require('path');
 
-// Apuntamos a la raíz del repositorio de Rancher
-const RANCHER_ROOT = path.resolve(__dirname, '..', '..'); 
+// Localizamos la raíz del repositorio (donde está la carpeta /shell)
+const RANCHER_ROOT = path.resolve(__dirname, '..', '..');
 
 module.exports = require('../../node_modules/@rancher/shell/pkg/vue.config')(__dirname, {
+  // 1. SOLUCIÓN AL ERROR ESLINT: Desactiva la validación de estilo al compilar
+  lintOnSave: false,
+
   chainWebpack(config) {
-    // Definimos la ruta de la librería de UI del shell, donde están los archivos .vue.
-    const SHELL_LIB = path.resolve(RANCHER_ROOT, 'node_modules/@rancher/shell/lib');
+    // 2. SOLUCIÓN AL ERROR DE ALIAS: 
+    // Apuntamos @shell a la carpeta física 'shell' en la raíz del proyecto
+    const shellPath = path.resolve(RANCHER_ROOT, 'shell');
 
     config.resolve.alias
-      .set('@shell', SHELL_LIB)
-      .set('~@shell', SHELL_LIB);
+      .set('@shell', shellPath)
+      .set('~@shell', shellPath);
+
+    // Opcional: Si usas componentes de @components, asegúrate que también resuelvan
+    config.resolve.alias
+      .set('@pkg', path.resolve(__dirname))
+      .set('~@pkg', path.resolve(__dirname));
   }
 });
