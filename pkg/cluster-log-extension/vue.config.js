bcfc71659e0c3b36ddfcfc643a08c85b3a5d4fc7
mgrinbergpractiauy <130935328+mgrinbergpractiauy@users.noreply.github.com>
2025-12-17 09:43:43 -0300

    Refactor Vue config for path aliasing
    Updated path aliasing for @shell and added alias for @pkg.

--- pkg/cluster-log-extension/vue.config.js
+++ pkg/cluster-log-extension/vue.config.js
@@ -1,16 +1,23 @@
 const path = require('path');
 
-// La raíz del repositorio donde está la carpeta /shell
+// Esta es la raíz del repositorio de Rancher
 const RANCHER_ROOT = path.resolve(__dirname, '..', '..');
 
 module.exports = require('../../node_modules/@rancher/shell/pkg/vue.config')(__dirname, {
+  // 1. Forzamos a que ignore errores de linter para que no rompa el build
   lintOnSave: false,
+
   chainWebpack(config) {
-    // Apuntamos @shell a la carpeta física de código de Rancher
-    const shellSource = path.resolve(RANCHER_ROOT, 'shell');
+    // 2. Definimos @shell apuntando a la carpeta real en el repo
+    const shellPath = path.resolve(RANCHER_ROOT, 'shell');
 
     config.resolve.alias
-      .set('@shell', shellSource)
-      .set('~@shell', shellSource);
+      .set('@shell', shellPath)
+      .set('~@shell', shellPath);
+
+    // 3. Opcional: Si usas componentes de @pkg, asegúrate que sepa que es esta carpeta
+    config.resolve.alias
+      .set('@pkg', __dirname)
+      .set('~@pkg', __dirname);
   }
 });
