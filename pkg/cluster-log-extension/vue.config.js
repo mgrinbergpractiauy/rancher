3c4621e40a13c9a5f8bfbd75e0e4d267a19f6e7f
mgrinbergpractiauy <130935328+mgrinbergpractiauy@users.noreply.github.com>
2025-12-17 10:09:09 -0300

    Update vue.config.js

--- pkg/cluster-log-extension/vue.config.js
+++ pkg/cluster-log-extension/vue.config.js
@@ -1,13 +1,23 @@
 const path = require('path');
 
 module.exports = require('../../node_modules/@rancher/shell/pkg/vue.config')(__dirname, {
-  lintOnSave: false, // Ignora errores de linter durante el build
+  lintOnSave: false,
   chainWebpack(config) {
-    // Calculamos la ruta real a la carpeta /shell que está 2 niveles arriba
-    const shellPath = path.resolve(__dirname, '../../shell');
+    // Definimos la ruta raíz del repositorio de Rancher
+    // __dirname es pkg/cluster-log-extension, así que subimos 2 niveles
+    const rootContext = path.resolve(__dirname, '../../');
+    const shellPath = path.join(rootContext, 'shell');
 
+    // Forzamos a Webpack a que use estas rutas para los alias
     config.resolve.alias
       .set('@shell', shellPath)
-      .set('~@shell', shellPath);
+      .set('~@shell', shellPath)
+      .set('@pkg', __dirname)
+      .set('~@pkg', __dirname);
+
+    // Esto ayuda a que Webpack busque en las carpetas de node_modules correctas
+    config.resolve.modules
+      .add(path.join(rootContext, 'node_modules'))
+      .add('node_modules');
   }
 });
