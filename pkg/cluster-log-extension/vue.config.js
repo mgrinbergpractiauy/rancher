474f5bd38464b9db6516e2d90b53ea512ea0ba1f
mgrinbergpractiauy <130935328+mgrinbergpractiauy@users.noreply.github.com>
2025-12-17 09:18:20 -0300

    Update vue.config.js

--- pkg/cluster-log-extension/vue.config.js
+++ pkg/cluster-log-extension/vue.config.js
@@ -1,1 +1,16 @@
-module.exports = require('../../node_modules/@rancher/shell/pkg/vue.config')(__dirname);
+// pkg/cluster-log-extension/vue.config.js
+const path = require('path');
+
+// Apuntamos a la raíz del repositorio de Rancher
+const RANCHER_ROOT = path.resolve(__dirname, '..', '..'); 
+
+module.exports = require('../../node_modules/@rancher/shell/pkg/vue.config')(__dirname, {
+  chainWebpack(config) {
+    // Definimos la ruta de la librería de UI del shell, donde están los archivos .vue.
+    const SHELL_LIB = path.resolve(RANCHER_ROOT, 'node_modules/@rancher/shell/lib');
+
+    config.resolve.alias
+      .set('@shell', SHELL_LIB)
+      .set('~@shell', SHELL_LIB);
+  }
+});
