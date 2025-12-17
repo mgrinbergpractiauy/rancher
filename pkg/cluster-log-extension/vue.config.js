af7ac65db049fb07a9d856e90068bf02e5a07b5c
mgrinbergpractiauy <130935328+mgrinbergpractiauy@users.noreply.github.com>
2025-12-17 09:29:02 -0300

    Update vue.config.js

--- pkg/cluster-log-extension/vue.config.js
+++ pkg/cluster-log-extension/vue.config.js
@@ -1,24 +1,16 @@
 const path = require('path');
 
-// Localizamos la raíz del repositorio (donde está la carpeta /shell)
+// La raíz del repositorio donde está la carpeta /shell
 const RANCHER_ROOT = path.resolve(__dirname, '..', '..');
 
 module.exports = require('../../node_modules/@rancher/shell/pkg/vue.config')(__dirname, {
-  // 1. SOLUCIÓN AL ERROR ESLINT: Desactiva la validación de estilo al compilar
   lintOnSave: false,
-
   chainWebpack(config) {
-    // 2. SOLUCIÓN AL ERROR DE ALIAS: 
-    // Apuntamos @shell a la carpeta física 'shell' en la raíz del proyecto
-    const shellPath = path.resolve(RANCHER_ROOT, 'shell');
+    // Apuntamos @shell a la carpeta física de código de Rancher
+    const shellSource = path.resolve(RANCHER_ROOT, 'shell');
 
     config.resolve.alias
-      .set('@shell', shellPath)
-      .set('~@shell', shellPath);
-
-    // Opcional: Si usas componentes de @components, asegúrate que también resuelvan
-    config.resolve.alias
-      .set('@pkg', path.resolve(__dirname))
-      .set('~@pkg', path.resolve(__dirname));
+      .set('@shell', shellSource)
+      .set('~@shell', shellSource);
   }
 });
