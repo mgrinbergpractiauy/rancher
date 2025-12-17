8dd46e17167e059fc694ff4deb27c4f0fd0d4914
mgrinbergpractiauy <130935328+mgrinbergpractiauy@users.noreply.github.com>
2025-12-17 10:25:59 -0300

    Update vue.config.js

--- pkg/cluster-log-extension/vue.config.js
+++ pkg/cluster-log-extension/vue.config.js
@@ -1,19 +1,21 @@
 const path = require('path');
 
-// Localizamos la carpeta raíz del repositorio (rancher/rancher)
-const root = path.resolve(__dirname, '..', '..');
-const shellPath = path.join(root, 'shell');
+// Localizamos la raíz del repositorio de forma absoluta
+const rootContext = path.resolve(__dirname, '../../');
+const shellPath = path.join(rootContext, 'shell');
 
 module.exports = require('../../node_modules/@rancher/shell/pkg/vue.config')(__dirname, {
   lintOnSave: false,
   chainWebpack(config) {
-    // 1. Forzar alias con rutas absolutas del sistema de archivos de GitHub
+    // 1. Forzamos la resolución de alias a rutas absolutas del servidor
     config.resolve.alias
       .set('@shell', shellPath)
-      .set('~@shell', shellPath);
+      .set('~@shell', shellPath)
+      .set('@pkg', __dirname)
+      .set('~@pkg', __dirname);
 
-    // 2. IMPORTANTE: Permitir que el cargador de Vue procese la carpeta /shell
-    // Sin esto, Webpack encuentra el archivo pero se niega a leerlo
+    // 2. IMPORTANTE: El compilador de Vue y Babel ignoran archivos fuera de la carpeta actual
+    // por defecto. Aquí obligamos a que procese la carpeta 'shell'.
     config.module
       .rule('vue')
       .include
@@ -21,9 +23,16 @@ module.exports = require('../../node_modules/@rancher/shell/pkg/vue.config')(__d
         .add(__dirname)
         .end();
 
-    // 3. Asegurar que resuelva las dependencias desde la raíz si no están en la extensión
+    // 3. También incluimos la carpeta shell en la regla de JS/TS para que Babel la procese
+    config.module
+      .rule('js')
+      .include
+        .add(shellPath)
+        .end();
+
+    // 4. Aseguramos que los node_modules se busquen también en la raíz
     config.resolve.modules
-      .add(path.join(root, 'node_modules'))
+      .add(path.join(rootContext, 'node_modules'))
       .add('node_modules');
   }
 });
