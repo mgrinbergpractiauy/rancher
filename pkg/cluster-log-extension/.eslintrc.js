d066e0beab6c07e2ee05012a660d2f183150df4b
mgrinberg.practia.gmail.com@gmail.com <mgrinberg@gmail.com>
2025-12-17 09:30:55 -0300

    asd

---
+++ pkg/cluster-log-extension/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  root: true, // Esto le dice: "no busques configuraciones fuera de esta carpeta"
+  env: { node: true },
+  extends: [ 'plugin:vue/essential' ],
+  rules: { 'no-console': 'off' }
+};
